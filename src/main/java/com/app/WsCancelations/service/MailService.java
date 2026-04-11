4251aa083653ea6a6fecf46169f62f6073be27f2
faiberRomero014 <faibera015@gmail.com>
2026-04-11 16:59:44 -0500

    service

---
+++ src/main/java/com/app/WsCancelations/service/MailService.java
@@ -0,0 +1,28 @@
+package com.app.WsCancelations.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+    @Value("${app.mail.from}")
+    private String from;
+
+
+    public void sendSimpleMail(String[] to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+
+        try {
+            mailSender.send(message);
+            log.info(" Se ha enviado un email simple exitosamente a {}", (Object) to);
+        } catch (Exception e) {
+            log.error("Error al intentar enviar un email simple: {}", e.getMessage(), e);
+        }
+    }
+}
