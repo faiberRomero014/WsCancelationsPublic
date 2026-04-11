4436ec74d9325e8e43af32fb6da1d20b86b6fc80
faiberRomero014 <faibera015@gmail.com>
2026-04-11 17:00:15 -0500

    service

--- src/main/java/com/app/WsCancelations/service/MailService.java
+++ src/main/java/com/app/WsCancelations/service/MailService.java
@@ -7,6 +7,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
+@Service
+@Slf4j
+public class MailService {
     @Value("${app.mail.from}")
     private String from;
 
