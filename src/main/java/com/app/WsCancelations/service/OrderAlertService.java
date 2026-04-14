105d7427f15cf943a60535e434cd3fd6488dfa2b
faiberRomero014 <faibera015@gmail.com>
2026-04-14 16:29:17 -0500

    Update OrderAlertService.java

--- src/main/java/com/app/WsCancelations/service/OrderAlertService.java
+++ src/main/java/com/app/WsCancelations/service/OrderAlertService.java
@@ -1,6 +1,10 @@
 package com.app.WsCancelations.service;
+
 import com.app.WsCancelations.utils.Constantes;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 @Log4j2
 @Service
 public class OrderAlertService {
